9be5aa4ddd3a3f0c4fd1a3b03a65928b1020ac10
mbayeDiao <mbayediao@mdiao.mbaye>
2024-11-29 19:30:38 +0000

    reducing_img

--- ray-tracing/src/main.rs
+++ ray-tracing/src/main.rs
@@ -55,76 +55,76 @@ fn ray_color(r: &Ray, world: &dyn Hittable, depth: i32) -> Color {
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
-fn random_scene() -> HittableList {
-    let mut world = HittableList::new();
+// fn random_scene() -> HittableList {
+//     let mut world = HittableList::new();
  
-    let ground_material = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
-    world.add(Box::new(Sphere::new(
-        Point3::new(0.0, -1000.0, 0.0),
-        1000.0,
-        ground_material,
-    )));
+//     let ground_material = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+//     world.add(Box::new(Sphere::new(
+//         Point3::new(0.0, -1000.0, 0.0),
+//         1000.0,
+//         ground_material,
+//     )));
  
-    for a in -5..5 {
-        for b in -5..5 {
-            let choose_mat = common::random_double();
-            let center = Point3::new(
-                a as f64 + 0.9 * common::random_double(),
-                0.2,
-                b as f64 + 0.9 * common::random_double(),
-            );
+//     for a in -5..5 {
+//         for b in -5..5 {
+//             let choose_mat = common::random_double();
+//             let center = Point3::new(
+//                 a as f64 + 0.9 * common::random_double(),
+//                 0.2,
+//                 b as f64 + 0.9 * common::random_double(),
+//             );
  
-            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
-                if choose_mat < 0.8 {
-                    // Diffuse
-                    let albedo = Color::random() * Color::random();
-                    let sphere_material = Rc::new(Lambertian::new(albedo));
-                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
-                } else if choose_mat < 0.95 {
-                    // Metal
-                    let albedo = Color::random_range(0.5, 1.0);
-                    let fuzz = common::random_double_range(0.0, 0.5);
-                    let sphere_material = Rc::new(Metal::new(albedo, fuzz));
-                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
-                } else {
-                    // Glass
-                    let sphere_material = Rc::new(Dielectric::new(1.5));
-                    world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
-                }
-            }
-        }
-    }
+//             if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+//                 if choose_mat < 0.8 {
+//                     // Diffuse
+//                     let albedo = Color::random() * Color::random();
+//                     let sphere_material = Rc::new(Lambertian::new(albedo));
+//                     world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+//                 } else if choose_mat < 0.95 {
+//                     // Metal
+//                     let albedo = Color::random_range(0.5, 1.0);
+//                     let fuzz = common::random_double_range(0.0, 0.5);
+//                     let sphere_material = Rc::new(Metal::new(albedo, fuzz));
+//                     world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+//                 } else {
+//                     // Glass
+//                     let sphere_material = Rc::new(Dielectric::new(1.5));
+//                     world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+//                 }
+//             }
+//         }
+//     }
  
-    let material1 = Rc::new(Dielectric::new(1.5));
-    world.add(Box::new(Sphere::new(
-        Point3::new(0.0, 1.0, 0.0),
-        1.0,
-        material1,
-    )));
+//     let material1 = Rc::new(Dielectric::new(1.5));
+//     world.add(Box::new(Sphere::new(
+//         Point3::new(0.0, 1.0, 0.0),
+//         1.0,
+//         material1,
+//     )));
  
-    let material2 = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
-    world.add(Box::new(Sphere::new(
-        Point3::new(-4.0, 1.0, 0.0),
-        1.0,
-        material2,
-    )));
+//     let material2 = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+//     world.add(Box::new(Sphere::new(
+//         Point3::new(-4.0, 1.0, 0.0),
+//         1.0,
+//         material2,
+//     )));
  
-    let material3 = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
-    world.add(Box::new(Sphere::new(
-        Point3::new(4.0, 1.0, 0.0),
-        1.0,
-        material3,
-    )));
+//     let material3 = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+//     world.add(Box::new(Sphere::new(
+//         Point3::new(4.0, 1.0, 0.0),
+//         1.0,
+//         material3,
+//     )));
  
-    world
-}
+//     world
+// }
 
 fn main() {
     // Image
 
     // Définit un ratio d’aspect de 16:9 (largeur/hauteur).
     const ASPECT_RATIO: f64 = 3.0 / 2.0;
-    const IMAGE_WIDTH: i32 = 400;
+    const IMAGE_WIDTH: i32 = 100;
 
     //  Dimensions de l'image en pixels. Ici, largeur = 400 pixels, hauteur = 400/(16/9)=225 pixels.
     const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
@@ -132,9 +132,34 @@ fn main() {
     const MAX_DEPTH: i32 = 50;
 
     // World
-    let world = random_scene();
     
-   
+    let mut world = HittableList::new();
+ 
+    let ground_material = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    let metal = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+    let _diffus = Rc::new(Lambertian::new(Color::new(0.2,0.1,0.3)));
+    let _glass = Rc::new(Dielectric::new(1.4));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
+    )));
+
+    world.add(Box::new(Sphere::new(
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        metal.clone(),
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        metal.clone(),
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(8.0, 1.0, 0.0),
+        1.0,
+        metal,
+    )));
     // Camera
     
     let lookfrom = Point3::new(13.0, 2.0, 3.0);
