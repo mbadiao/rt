a67548b909c6258c43fffc6ee757b5f1657aaef1
sefaye <wiwadev@gmail.com>
2024-12-29 01:28:06 +0000

    maj

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,25 @@
-use std::f64::consts::PI;
 use std::fs::File;
+use std::f64::consts::PI;
 use std::io::Write;
-
 use rt::camera::*;
 use rt::light::*;
 use rt::ray::*;
 use rt::vec3::*;
 use rt::world::*;
-
-use rt::cube::*;
 use rt::cylindre::*;
 use rt::plane::*;
 use rt::sphere::*;
 use rt::cube::*;
 use indicatif::{ProgressBar,ProgressStyle};
-
 // fn ray_color(ray: &Ray, world: &World, lights: &[Light]) -> Vec3 {
 //     let background_color = Vec3::new(0.5, 0.7, 1.0);
-
 //     match world.hit(ray, 0.001, f64::INFINITY) {
 //         Some(hit_record) => {
 //             let mut total_color = Vec3::new(0.0, 0.0, 0.0);
-
 //             for light in lights {
 //                 let light_color = calculate_lighting(&hit_record, light);
 //                 total_color = total_color.add(&light_color);
 //             }
-
 //             total_color
 //         }
 //         None => {
@@ -38,8 +31,6 @@ use indicatif::{ProgressBar,ProgressStyle};
 //         }
 //     }
 // }
-
-
 fn ray_color(ray: &Ray, world: &World, lights: &[Light]) -> Vec3 {
     let background_color = Vec3::new(0.5, 0.7, 1.0);
     
@@ -64,13 +55,10 @@ fn ray_color(ray: &Ray, world: &World, lights: &[Light]) -> Vec3 {
         }
     }
 }
-
-
 fn main() -> std::io::Result<()> {
     let width = 800;
     let height = 600;
     let samples = 10;
-
     let mut file = File::create("world_scene.ppm")?;
     writeln!(file, "P3\n{} {}\n255", width, height)?;
     // Creation de la barre de progression
@@ -79,34 +67,27 @@ fn main() -> std::io::Result<()> {
     .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} pixels ({eta})")
     .unwrap()
     .progress_chars("=>-"));
-
     // Création du monde avec des sphères aléatoires
     // let world = World::random_spheres(); // Génère 20 sphères aléatoires
-
     // Création du World et ajout des objets
     let mut world = World::new();
-
        // Ajout de plans
        world.add(Box::new(Plane::new(
         Vec3::new(0.0, -0.5, 0.0),
         Vec3::new(0.0, 1.0, 0.0),
         Vec3::new(0.5, 0.5, 0.5),
     )));
-
-
     // // Ajout de plusieurs sphères
     // world.add(Box::new(Sphere::new(
     //     Vec3::new(0.0, 0.0, -1.5),
     //     0.5,
     //     Vec3::new(0.8, 0.3, 0.3),
     // )));
-
     world.add(Box::new(Sphere::new(
         Vec3::new(2.5, 0.2, -1.0),
         0.8,
         Vec3::new(1.0, 1.0, 0.0),
     )));
-
     
     // Ajout de cylindres
     world.add(Box::new(Cylinder::new(
@@ -116,38 +97,23 @@ fn main() -> std::io::Result<()> {
         1.0,                        // Hauteur
         Vec3::new(0.3, 0.3, 0.8),   // Couleur
     )));
-
-
     // Ajout d'un cube avec des dimensions plus visibles
-    let mut cube = Cube::new(
-        Vec3::new(-1.0, -0.5, -2.0), // Point minimum
-        Vec3::new(0.0, 0.5, -1.0),   // Point maximum
-        Vec3::new(0.8, 0.6, 0.2),    // Couleur (doré)
+    let mut cube =  Cube::new(
+        Vec3::new(-1.0, -0.5, -2.0),    // Point minimum
+        Vec3::new(0.0, 0.5, -1.0),      // Point maximum
+        Vec3::new(0.8, 0.6, 0.2),       // Couleur (doré)
     );
-    // Vecteur de déplacement : déplace le cube de 2 unités vers la droite et 1 unité vers le haut
-    let offset = Vec3::new(2.0, 1.0, 0.0);
-    cube.translate(offset);
+    // cube.rotate_y(PI/2.0);
 
     world.add(Box::new(cube));
 
+    
     // Création des lumières
-    // let lights = vec![
-    //     Light::new(Vec3::new(5.0, 5.0, -3.0), 0.8),
-    //     Light::new(Vec3::new(-5.0, 5.0, -3.0), 0.6),
-    // ];
-
-    // Création des lumières
-    // let lights = vec![
-    //     Light::new(Vec3::new(-10.0, 0.1, -10.0), 0.8), // Lumière rasante depuis l'horizon
-    //     Light::new(Vec3::new(10.0, 0.1, -10.0), 0.6),  // Autre lumière rasante pour équilibre
-    // ];
-
     let lights = vec![
         Light::new(Vec3::new(5.0, 5.0, -3.0), 0.8),
         // Light::new(Vec3::new(-5.0, 5.0, -3.0), 0.6),
         // Light::new(Vec3::new(0.0, 5.0, 0.0), 0.4),
     ];
-
     let camera = Camera::new(
         Vec3::new(3.0, 2.0, 6.0),     // Position: légèrement en hauteur et en retrait
         Vec3::new(0.0, 0.0, -1.0),    // Point de visée: centre de la scène
@@ -163,27 +129,20 @@ fn main() -> std::io::Result<()> {
     //     35.0,                          // Angle plus étroit pour un effet plus cinématographique
     //     (width as f64) / (height as f64),
     // );
-
-
-
     for j in (0..height).rev() {
         for i in 0..width {
             let mut pixel_color = Vec3::new(0.0, 0.0, 0.0);
-
             for _ in 0..samples {
                 let u = (i as f64 + rand::random::<f64>()) / (width as f64);
                 let v = (j as f64 + rand::random::<f64>()) / (height as f64);
                 let ray = camera.get_ray(u, v);
                 pixel_color = pixel_color.add(&ray_color(&ray, &world, &lights));
             }
-
             pixel_color = pixel_color.mul(1.0 / samples as f64);
             let ir = (255.99 * pixel_color.x.sqrt()) as u8;
             let ig = (255.99 * pixel_color.y.sqrt()) as u8;
             let ib = (255.99 * pixel_color.z.sqrt()) as u8;
-
             writeln!(file, "{} {} {}", ir, ig, ib).unwrap();
-
             // Incrémenter la barre de progression
             progress_bar.inc(1);
         }
